318bb206b02de2b57879b3afdd49f1c41b87398d
muffler01 <muffler01@naver.com>
2021-02-07 11:34:04 +0900

    2d platformer game code

---
+++ 2D Platformer Game/GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+
+    public int health;
+    public PlayerMove player;
+    public GameObject[] stages;
+
+    public Image[] UIhealth;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject UIRetryButton;
+
+    void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
+    public void NextStage()
+    {
+        // Change Stage
+        if (stageIndex < stages.Length - 1)
+        {
+            stages[stageIndex].SetActive(false);
+            stageIndex++;
+            stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            UIStage.text = "STAGE " + (stageIndex + 1);
+        }
+        // Game Clear
+        else
+        {
+            // Player Control Lock
+            Time.timeScale = 0;
+            // Result UI
+            Debug.Log("Game Clear!");
+            // Restart Button UI
+            UIRetryButton.SetActive(true);
+            Text buttonText = UIRetryButton.GetComponentInChildren<Text>();
+            buttonText.text = "Clear!";
+            UIRetryButton.SetActive(true);
+        }
+
+        // Calculate Point
+        totalPoint += stagePoint;
+        stagePoint = 0;
+    }
+    public void HealthDown()
+    {
+        if (health > 1)
+        {
+            health--;
+            UIhealth[health].color = new Color(1, 1, 1, 0.4f);
+        }
+
+        else
+        {
+            // All Health UI Off
+            UIhealth[0].color = new Color(1, 1, 1, 0.4f);
+            // Player Die Effect
+            player.OnDie();
+            // Result UI
+            Debug.Log("Game Over");
+            // Retry Button UI
+            UIRetryButton.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Respawn
+            if (health > 1)
+            {
+                PlayerReposition();
+            }
+
+            // Health Down
+            HealthDown();
+        }
+    }
+
+    void PlayerReposition()
+    {
+        player.transform.position = new Vector3(-5, 0, -1);
+        player.VelocityZero();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ 2D Platformer Game/MonsterAI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAI : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    CapsuleCollider2D capsulecollider;
+
+    public int nextMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capsulecollider = GetComponent<CapsuleCollider2D>();
+
+        Invoke("Think", 5);
+    }
+
+    void FixedUpdate()
+    {
+        // [Move Function]
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        // [PlatformCheck Function]
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.4f, rigid.position.y);
+
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+    }
+
+    void Think()
+    {
+        // [NextActive Function]
+        nextMove = Random.Range(-1, 2);
+
+        // [SpriteAnimation Function]
+        animator.SetInteger("WalkSpeed", nextMove);
+        
+        // [FlipSprite Function]
+        if (nextMove != 0)
+            spriteRenderer.flipX = nextMove == 1;
+
+        // [Recursive Function]
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove = nextMove * -1;
+        spriteRenderer.flipX = nextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 2);
+    }
+
+    // [OnDamaged Function]
+    public void OnDamaged()
+    {
+        // Sprite Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        // Sprite Flip y
+        spriteRenderer.flipY = true;
+        // Collider Disable
+        capsulecollider.enabled = false;
+        // Die Effect Jump
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        // Destroy
+        Invoke("DeActive", 5);
+    }
+
+    // [DeActive Function]
+    void DeActive()
+    {
+        gameObject.SetActive(false);
+    }
+}
