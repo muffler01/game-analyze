1e3766a80625953b8a1834a560e093bdb2983c70
muffler01 <muffler01@naver.com>
2021-02-09 18:51:37 +0900

    pratice

--- 2D Platformer Game/PlayerMove.cs
+++ 2D Platformer Game/PlayerMove.cs
@@ -4,216 +4,126 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public GameManager gameManager;
+    /* 공격 : z
+     * 방어 : ↓
+     * 이동 : ←→
+     * 점프 : x
+     */
 
-    public AudioClip audioJump;
-    public AudioClip audioAttack;
-    public AudioClip audioDamaged;
-    public AudioClip audioItem;
-    public AudioClip audioDie;
-    public AudioClip audioFinish;
+    // variable Define
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
 
     public float maxSpeed;
     public float jumpPower;
 
-    Rigidbody2D rigid;
-    SpriteRenderer spriteRenderer;
-    Animator animator;
-    CapsuleCollider2D capsuleCollider;
-    AudioSource audioSource;
-
+    // variable Reset
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-        capsuleCollider = GetComponent<CapsuleCollider2D>();
-        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        // [Jump Function]
-        if (Input.GetButtonDown("Jump") && !animator.GetBool("isJumping"))
-        {
-            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            animator.SetBool("isJumping", true);
-            PlaySound("JUMP");
-            audioSource.Play();
-        }
-
-        // [Stop Speed Function]
-        if (Input.GetButtonUp("Horizontal"))
-        {
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
-        }
-
-        // [Direction Sprite Function]
-        if (Input.GetButton("Horizontal"))
-            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-
-        // [Walk Animation Function]
-        if (Mathf.Abs(rigid.velocity.x) < 0.3)
-            animator.SetBool("isWalking", false);
-        else
-            animator.SetBool("isWalking", true);
+        Run();
+        Jump();
+        Fall();
+        Block();
+        Roll();
+        SpriteController();
     }
 
     void FixedUpdate()
     {
-        // [Movement Function]
-        float h = Input.GetAxisRaw("Horizontal");
+        Movement();
+    }
 
+    void Movement()
+    {
+        // Movement Function
+        float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h * 2, ForceMode2D.Impulse);
 
-        // [Max Speed Function]
-        if (rigid.velocity.x > maxSpeed) // Right Max Speed
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        else if (rigid.velocity.x < maxSpeed * (-1)) // Left Max Speed
-            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        // Max Speed Function
+        if (rigid.velocity.x != 0)
+        {
+            if (rigid.velocity.x > maxSpeed)
+                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+            else if (rigid.velocity.x < maxSpeed * (-1))
+                rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        }
 
-        // [Landing Platform Function]
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            animator.SetInteger("AnimState", 0);
+        else
+            animator.SetInteger("AnimState", 1);
+    }
+
+    void Jump()
+    {
+        // Jump Function
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    void Fall()
+    {
+        // Fall Function
         if (rigid.velocity.y < 0)
         {
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
-            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
-
-            if (rayHit.collider != null)
-                if (rayHit.distance < 0.5f)
+            RaycastHit2D raycastHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (raycastHit.collider == null)
+            {
+                animator.SetFloat("AirSpeedY", -1);
+                animator.SetBool("Grounded", false);
+            }
+            else if (raycastHit.collider != null)
+                if (raycastHit.distance < 0.3f)
+                {
+                    animator.SetFloat("AirSpeedY", 1);
+                    animator.SetBool("Grounded", true);
                     animator.SetBool("isJumping", false);
+                }
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void Block()
     {
-        if (collision.gameObject.tag == "Enemy")
-        {   
-            // Attack
-            if (rigid.velocity.y < 0 && transform.position.y > collision.transform.position.y)
-            {
-                OnAttack(collision.transform);
-                PlaySound("ATTACK");
-                audioSource.Play();
-            }
-            // Damaged
-            else
-            {
-                OnDamaged(collision.transform.position);
-                PlaySound("DAMAGED");
-                audioSource.Play();
-            }
-        }
+        if (Input.GetButtonDown("Block"))
+            animator.SetTrigger("Block");
+        else if (Input.GetButton("Block"))
+            animator.SetBool("IdleBlock", true);
+        else if (Input.GetButtonUp("Block"))
+            animator.SetBool("IdleBlock", false);
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void Roll()
     {
-        if (collision.gameObject.tag == "Item")
-        {
-            // Point
-            bool isBronze = collision.gameObject.name.Contains("Bronze");
-            bool isSilver = collision.gameObject.name.Contains("Silver");
-            bool isGold = collision.gameObject.name.Contains("Gold");
-            if (isBronze)
-                gameManager.stagePoint += 50;
-            else if (isSilver)
-                gameManager.stagePoint += 100;
-            else if (isGold)
-                gameManager.stagePoint += 300;
-
-            // DeActive Item
-            collision.gameObject.SetActive(false);
-            PlaySound("ITEM");
-            audioSource.Play();
-        }
-        else if (collision.gameObject.tag == "Finish")
-        {
-            // Next Stage
-            gameManager.NextStage();
-            PlaySound("FINISH");
-            audioSource.Play();
-        }
+        if (Input.GetButtonDown("Roll"))
+            animator.SetTrigger("Roll");
     }
 
-    // [OnAttack Function]
-    void OnAttack(Transform monster)
+    void SpriteController()
     {
-        // Point
-        gameManager.stagePoint += 100;
+        // Stop Speed Function
+        if (Input.GetButtonUp("Horizontal"))
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
 
-        // Reaction Force
-        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
-        // Enemy Die
-        MonsterAI monsterAI = monster.GetComponent<MonsterAI>();
-        monsterAI.OnDamaged();
-    }
-
-    // [OnDamaged Function]
-    void OnDamaged(Vector2 targetPos)
-    {
-        // Health Down
-        gameManager.HealthDown();
-        // Change Layer
-        gameObject.layer = 11;
-        // View Alpha
-        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
-        // Reaction Force
-        int damDirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
-        rigid.AddForce(new Vector2(damDirc, 1) * 7, ForceMode2D.Impulse);
-        // Animation
-        animator.SetTrigger("doDamaged");
-
-        Invoke("OffDamaged", 2);
-    }
-
-    // [OffDamaged Function]
-    void OffDamaged()
-    {
-        gameObject.layer = 10;
-        spriteRenderer.color = new Color(1, 1, 1, 1);
-    }
-
-    public void OnDie()
-    {
-        // Sprite Alpha
-        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
-        // Sprite Flip y
-        spriteRenderer.flipY = true;
-        // Collider Disable
-        capsuleCollider.enabled = false;
-        // Die Effect Jump
-        rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-        PlaySound("DIE");
-        audioSource.Play();
-    }
-
-    public void VelocityZero()
-    {
-        rigid.velocity = Vector2.zero;
-    }
-
-    void PlaySound(string action)
-    {
-        switch (action)
-        {
-            case "JUMP":
-                audioSource.clip = audioJump;
-                break;
-            case "ATTACK":
-                audioSource.clip = audioAttack;
-                break;
-            case "DAMAGED":
-                audioSource.clip = audioDamaged;
-                break;
-            case "ITEM":
-                audioSource.clip = audioItem;
-                break;
-            case "DIE":
-                audioSource.clip = audioDie;
-                break;
-            case "FINISH":
-                audioSource.clip = audioFinish;
-                break;
-
-        }
+        // Direction Sprite Function
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
     }
 }
+
---
+++ 2D Platformer Game/PlayerMove2.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    public AudioClip audioJump;
+    public AudioClip audioAttack;
+    public AudioClip audioDamaged;
+    public AudioClip audioItem;
+    public AudioClip audioDie;
+    public AudioClip audioFinish;
+
+    public float maxSpeed;
+    public float jumpPower;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    CapsuleCollider2D capsuleCollider;
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // [Jump Function]
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+            PlaySound("JUMP");
+            audioSource.Play();
+        }
+
+        // [Stop Speed Function]
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        // [Direction Sprite Function]
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        // [Walk Animation Function]
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            animator.SetBool("isWalking", false);
+        else
+            animator.SetBool("isWalking", true);
+    }
+
+    void FixedUpdate()
+    {
+        // [Movement Function]
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * h * 2, ForceMode2D.Impulse);
+
+        // [Max Speed Function]
+        if (rigid.velocity.x > maxSpeed) // Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1)) // Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        // [Landing Platform Function]
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+                if (rayHit.distance < 0.5f)
+                    animator.SetBool("isJumping", false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {   
+            // Attack
+            if (rigid.velocity.y < 0 && transform.position.y > collision.transform.position.y)
+            {
+                OnAttack(collision.transform);
+                PlaySound("ATTACK");
+                audioSource.Play();
+            }
+            // Damaged
+            else
+            {
+                OnDamaged(collision.transform.position);
+                PlaySound("DAMAGED");
+                audioSource.Play();
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            // Point
+            bool isBronze = collision.gameObject.name.Contains("Bronze");
+            bool isSilver = collision.gameObject.name.Contains("Silver");
+            bool isGold = collision.gameObject.name.Contains("Gold");
+            if (isBronze)
+                gameManager.stagePoint += 50;
+            else if (isSilver)
+                gameManager.stagePoint += 100;
+            else if (isGold)
+                gameManager.stagePoint += 300;
+
+            // DeActive Item
+            collision.gameObject.SetActive(false);
+            PlaySound("ITEM");
+            audioSource.Play();
+        }
+        else if (collision.gameObject.tag == "Finish")
+        {
+            // Next Stage
+            gameManager.NextStage();
+            PlaySound("FINISH");
+            audioSource.Play();
+        }
+    }
+
+    // [OnAttack Function]
+    void OnAttack(Transform monster)
+    {
+        // Point
+        gameManager.stagePoint += 100;
+
+        // Reaction Force
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        // Enemy Die
+        MonsterAI monsterAI = monster.GetComponent<MonsterAI>();
+        monsterAI.OnDamaged();
+    }
+
+    // [OnDamaged Function]
+    void OnDamaged(Vector2 targetPos)
+    {
+        // Health Down
+        gameManager.HealthDown();
+        // Change Layer
+        gameObject.layer = 11;
+        // View Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        // Reaction Force
+        int damDirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(damDirc, 1) * 7, ForceMode2D.Impulse);
+        // Animation
+        animator.SetTrigger("doDamaged");
+
+        Invoke("OffDamaged", 2);
+    }
+
+    // [OffDamaged Function]
+    void OffDamaged()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
+
+    public void OnDie()
+    {
+        // Sprite Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        // Sprite Flip y
+        spriteRenderer.flipY = true;
+        // Collider Disable
+        capsuleCollider.enabled = false;
+        // Die Effect Jump
+        rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        PlaySound("DIE");
+        audioSource.Play();
+    }
+
+    public void VelocityZero()
+    {
+        rigid.velocity = Vector2.zero;
+    }
+
+    void PlaySound(string action)
+    {
+        switch (action)
+        {
+            case "JUMP":
+                audioSource.clip = audioJump;
+                break;
+            case "ATTACK":
+                audioSource.clip = audioAttack;
+                break;
+            case "DAMAGED":
+                audioSource.clip = audioDamaged;
+                break;
+            case "ITEM":
+                audioSource.clip = audioItem;
+                break;
+            case "DIE":
+                audioSource.clip = audioDie;
+                break;
+            case "FINISH":
+                audioSource.clip = audioFinish;
+                break;
+
+        }
+    }
+}
