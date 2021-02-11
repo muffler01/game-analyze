299732c6f32f4117aa91e5593904858f8a7d6303
muffler01 <muffler01@naver.com>
2021-02-11 19:12:50 +0900

    code cs

---
+++ 2D Game/Call.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Call : MonoBehaviour
+{
+    public Image titleImg;
+    public GameObject pressAny;
+    public GameObject btnSet;
+
+    void CallPressAny()
+    {
+            pressAny.SetActive(true);      
+    }
+
+    void OffPressAny()
+    {
+        pressAny.SetActive(false);
+    }
+
+    void CallMenu()
+    {
+        btnSet.SetActive(true);
+    }
+}
---
+++ 2D Game/FadeCycle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeCycle : MonoBehaviour
+{
+    Animator animator;
+    public Animator animatorTitle;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+            Change();
+    }
+
+    void Cycle()
+    {
+        animator.SetBool("isCycling", true);
+    }
+
+    void Change()
+    {
+        if (Input.anyKeyDown && animator.GetBool("isCycling"))
+        {
+            // PressAny Exit
+            animator.SetBool("isCycling", false);
+            animator.SetTrigger("FadeOut");
+            animatorTitle.SetBool("Up", true);
+        }
+    }
+}
+
